5199d0e082c1996d415526371976f4a6178f8562
Elin Sohlberg Viman <elinsvt@hotmail.com>
2020-02-28 09:26:46 +0100

    Quality testing start ssim

---
+++ A-test/QualityTesting.m
@@ -0,0 +1,24 @@
+clc
+close all
+clear all
+
+orginalStarry = imread('starryNight.jpg');
+starryBiggest = imread('starryNight20colorsLeft.png');
+strarrySmallest = imread('starryNight20colorsLeftSmall.png');
+
+orginalSmall = imresize(orginalStarry, 0.7);
+
+[ssimval,ssimmap] = ssim(starryBiggest,orginalSmall);
+imshow(ssimmap,[])
+title(['Local SSIM Map with Global SSIM Value: ',num2str(ssimval)])
+
+[ssimval,ssimmap] = ssim(strarrySmallest,orginalSmall);
+figure;
+imshow(ssimmap,[])
+title(['Local SSIM Map with Global SSIM Value: ',num2str(ssimval)])
+
+%% Kod för att lägga till kontur på orginalbilden
+% a = i;
+% b = repmat(mask, 1, 1, 3);
+% a(mod(b,2)==1) = 0;
+% imshow(a);
